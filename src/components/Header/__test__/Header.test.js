23bee9158adec7453f95efb12e77221a8fd0cfb7
Nicola Hearn <Nicola.Hearn@EntainGroup.com>
2023-09-15 17:02:11 +0100

    first commit

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,6 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
+test('renders learn react link', async () => {
+    render(<Header title="My Header"/>);
+    const headingElement = screen.getByText(/my header/i);
+    expect(headingElement).toBeInTheDocument();
+})
+
 describe("Header", () => {
     it('should render same text passed into title prop', () => {
         render(
